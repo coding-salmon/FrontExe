7016d2c8e036e0f3b87f04a9dda8894b71043808
coding-salmon <ejzus88@naver.com>
2024-03-11 21:29:38 +0900

    토이프로젝트:클라이머키우기

---
+++ 연습/클라이머키우기/Js/script.js
@@ -0,0 +1,13 @@
+// 모달 창 표시 함수
+
+function showInstructions(){
+    
+    const modal =document.getElementById("modal");
+    modal.style.display="block";
+}
+
+// 모달 창 닫기 함수
+const closeModal = ()=>{
+    const modal =document.getElementById("modal");
+    modal.style.display="none"
+}
