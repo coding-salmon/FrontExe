df80f02c64648dd22a1f32c628fa0fc5c89a6ce9
coding-salmon <ejzus88@naver.com>
2024-03-11 21:58:59 +0900

    게임시작구성하기

--- 연습/클라이머키우기/Js/script.js
+++ 연습/클라이머키우기/Js/script.js
@@ -11,3 +11,24 @@ const closeModal = ()=>{
     const modal =document.getElementById("modal");
     modal.style.display="none"
 }
+
+function  shareGame() {
+    const url = window.location.href;
+
+    navigator.clipboard.writeText(url)
+    .then(function(){
+        alert("[복사완료]다른클라이머들에게 링크를 보내주세요!.")
+    })
+    .catch(function(){
+        alert("[복사실패] ㅠㅠ 수동으로 복사해서보내주세요! ")
+    })
+    
+}
+
+function exitGame(){
+    window.close();
+}
+
+function insta(){
+    window.location.href="https://www.instagram.com/0._.329";
+}
