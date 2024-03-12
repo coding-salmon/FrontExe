8e1886c486a65d111661999aa7c70eddd355fc5d
coding-salmon <ejzus88@naver.com>
2024-03-12 22:02:12 +0900

    게임종료 화면 구성

--- 연습/클라이머키우기/Js/script.js
+++ 연습/클라이머키우기/Js/script.js
@@ -26,7 +26,11 @@ function  shareGame() {
 }
 
 function exitGame(){
+    //사용자에게 확인 메시지를 표시하고, 확인 시 현재 창을 닫음
+    const confirmation  =confirm("정말 게임을 종료하시겠습니까?");
+    if(confirmation ){
     window.close();
+    }
 }
 
 // 모달창 열기
@@ -39,7 +43,7 @@ function closeClimbGameModal(){
 }
 
 function exitConfirm(){
-    window.close()
+        window.close();
 }
 
 function insta(){
@@ -48,13 +52,85 @@ function insta(){
 
 document.addEventListener("DOMContentLoaded", function(){
     const bgm = document.getElementById('bgm');
-    bgm.addEventListener('ended',function(){
-        this.currentTime=0;
-        this.play();
-    }, false);
-    bgm.play();
+    bgm.play().then(()=>{
+        //음소거를 해제하고 재생합니다.
+        bgm.muted =false;
+    }).catch((error) =>{
+        //오디오 재생에 실한 경우 처리할 내용
+        console.error('오디오 재생에 실패했습니다:', error);
+    }); 
 });
 
+
 function startGame(){
-    window.location.href="Climber-Tycoon.html";
+    window.open("climber-Tycoon.html");
+}
+
+//클라이밍 모달창 열기
+function startClimbing(){
+    document.getElementById("climbGameModal").style.display='block';
+}
+
+//모달창 닫기
+function closeGameModal(){
+    document.getElementById("climbGameModal").style.display='none';
+}
+
+//게임 오버 횟수를 저장할 변수 선언
+let gameOverCount =0;
+
+//클라이밍 모달창에서 "오늘은 컨디션이 좀 ... 쉰다!를 선택한 경우"
+function noClimb(){
+    if(gameOverCount <10) {
+        //초기 확률은 100%에서 시작하여 10%씩 감소합니다.
+        let probability =1 -(gameOverCount*0.1);
+    
+
+    //0부터 1사이의 무작위한 값 생성
+    const randomNumber = Math.random();
+
+    //현재 확률로 게임 오버 또는 모달창 닫기
+    if(randomNumber < probability){
+        //게임 오버 메시지와 점수 표시
+        alert("게임오버 - 클밍을 못가서 돌연사 \n 당신의 점수는 " + score+"입니다.");
+    
+
+    //점수 초기화
+    score = 0;
+
+    //게임 오버 횟수 증가
+    gameOverCount++;
+
+    //첫 화면으로 돌아가기
+    window.location.href = "../game-over.html"; //게임 오버 페이지로 이동
+}else{
+    //클라이밍 모달창 닫기
+    closeClimbGameModal();
+
+}
+}else{
+    //5% 확률로 게임 오버 또는 모달창 닫기
+
+    let randomNumber = Math.random();
+    if(randomNumber < 0.05){
+
+        //게임 오버 메시지와 점수 표시
+        alert("게임 오버 - 클라이밍을 너무 쉬었더니 돌연사  \n 당신의 점수는 "+score+
+        "입니다");
+
+        //점수 초기화
+        score = 0;
+
+        //게임 오버 횟수 증가
+        gameOverCount++;
+
+        //첫 화면으로 돌아가기
+        window.location.href="gameover.html"; //게임 오버 페이지 이동
+            
+    }else{
+            //클라이밍 모달창 닫기
+            closeClimbGameModal();
+        }
+
+    }
 }
