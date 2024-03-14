691a9bbeab14e284ff35a5949ca2e0c24bb1bce6
coding-salmon <ejzus88@naver.com>
2024-03-14 17:44:15 +0900

    채팅

--- WebSocket_Server/WebjavatServer/src/websocket/SimpleWebSocketServer.java
+++ WebSocket_Server/WebjavatServer/src/websocket/SimpleWebSocketServer.java
@@ -36,8 +36,42 @@ public class SimpleWebSocketServer extends WebSocketServer {
 	}
 
 	@Override
-	public void onMessage(WebSocket arg0, String arg1) {
-		// TODO Auto-generated method stub
+	public void onMessage(WebSocket conn, String message) {
+		System.out.println("Message from clinet: "+message);
+		
+		//json 문자열 -> json객체
+		JSONObject msgObj = new JSONObject(message);
+		String cmd = msgObj.getString("cmd"); // 패킷 종류를 구분할 수 있는 명령어를 제일 먼저 추출
+		
+		if(cmd.equals("login")) {
+			String id = msgObj.getString("id");
+			String pass = msgObj.getString("pass");
+			System.out.printf("로그인 id: %s pass: %s\n",id,pass);
+			
+//			DB에서 해당 id, pass가 있는지 확인하는 작업추가
+			
+			JSONObject ackObj = new JSONObject();
+			ackObj.put("cmd","login");
+			ackObj.put("result","ok");
+			conn.send(ackObj.toString()); //json 문자열로 변환되어 클라이언트한테 전송됨
+			
+		}else if(cmd.equals("allchat")) {
+			String id = msgObj.getString("id");
+			String msg = msgObj.getString("msg");
+			System.out.printf("채팅 id %s msg: %s\n", id,msg);
+			
+			//클라이언트한테 응답 전송
+			JSONObject ackObj = new JSONObject();
+			ackObj.put("cmd","allchat");
+			ackObj.put("result","ok");
+			conn.send(ackObj.toString());
+			
+			//전체 접속자한테 브로드캐스팅
+			for(WebSocket con : this.getConnections()) {
+				if(conn !=)
+			}
+			
+		}
 		
 	}
 
@@ -50,7 +84,7 @@ public class SimpleWebSocketServer extends WebSocketServer {
 		JSONObject ackObj = new JSONObject();
 		ackObj.put("cmd", "connect");
 		ackObj.put("result","Welcome to the Server!");
-		conn.send(ackObj.toString());
+		conn.send(ackObj.toString()); // 클라이언트한테 메시지 보내기 
 		
 	}
 
